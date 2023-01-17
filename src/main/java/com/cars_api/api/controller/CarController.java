85621adfd7a7cfbaf8b94f2a512896042373bbe2
CaioPedras <caio.vp@gmail.com>
2023-01-17 14:55:01 -0300

    CreateCar route

---
+++ src/main/java/com/cars_api/api/controller/CarController.java
@@ -0,0 +1,24 @@
+package com.cars_api.api.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.cars_api.api.dto.CarDTO;
+
+@RestController
+@RequestMapping("/")
+public class CarController {
+
+    @PostMapping
+    public void createCar(@RequestBody CarDTO req) {
+        System.out.println(req.modelo());
+        System.out.println(req.fabricante());
+        System.out.println(req.dataFabricacao());
+        System.out.println(req.valor());
+        System.out.println(req.anoModelo());
+
+    }
+}
