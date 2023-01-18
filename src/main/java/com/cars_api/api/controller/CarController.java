3254d8d1a01316a53484ac8cd830e6d53cec9dfa
CaioPedras <caio.vp@gmail.com>
2023-01-18 14:50:49 -0300

    fix routes

--- src/main/java/com/cars_api/api/controller/CarController.java
+++ src/main/java/com/cars_api/api/controller/CarController.java
@@ -1,5 +1,8 @@
 package com.cars_api.api.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -7,13 +10,24 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.cars_api.api.dto.CarDTO;
+import com.cars_api.api.model.Car;
+import com.cars_api.api.repository.CarRepository;
 
 @RestController
 @RequestMapping("/")
 public class CarController {
 
+    @Autowired
+    private CarRepository repository;
+
+    @GetMapping
+    public List<Car> listAll() {
+        return repository.findAll();
+    }
+
     @PostMapping
     public void createCar(@RequestBody CarDTO req) {
+        repository.save(new Car(req));
         System.out.println(req.modelo());
         System.out.println(req.fabricante());
         System.out.println(req.dataFabricacao());
